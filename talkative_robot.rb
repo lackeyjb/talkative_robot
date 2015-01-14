c411ff8a332f2b18d4d409889706d29a200fa5f8
Bryan Lackey <bryan.lackey@gmail.com>
2015-01-14 16:57:49 -0500

    corrected problems with age/gender conditionals

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -24,16 +24,18 @@ def users_age_case(user_age)
 	end
 end
 
-def male_or_female?(user_gender)
-	user_gender == "M" 
+def male?(gender)
+	gender == "M" || gender == "Male"
 end
 
-def is_user_young?(user_age)
-	user_age < 12
+def young_person_message(gender)
+	young_person_greeting = male?(gender) ? "boy" : "girl"
+	return "young #{young_person_greeting}"
 end
 
-def is_user_old?(user_age)
-	user_age > 100
+def great_great_grandparent_message(gender)
+	older_gender_greeting = male?(gender) ? "pa" : "ma"
+	return "great-great grand#{older_gender_greeting}"
 end
 
 users_name = get_users_name
@@ -41,18 +43,14 @@ users_name = get_users_name
 users_gender = get_users_gender
 
 users_age = get_users_age
-
-young_person = male_or_female?(users_gender) && is_user_young?(users_age) ? "young boy" : "young girl"
-
-old_person =  male_or_female?(users_gender) && is_user_old?(users_age) ? "great-great grandpa" : "great-great grandma"
-
+		
 puts "Hi #{users_name}, who is #{users_age} year(s) old!"
 puts "Do you mind if I call you #{users_name.chars.first}?"
 
 users_age_case(users_age)
 
-puts "You are a #{young_person}." unless is_user_old?(users_age)
-puts "Are you a #{old_person}?" unless is_user_young?(users_age)
+puts "You're a #{young_person_message(users_gender)}." if users_age < 12
+puts "You're probably someone's #{great_great_grandparent_message(users_gender)}!!" if users_age >= 100
 
 puts "Hey #{users_name.upcase}, where are you going!?"
 puts 'Hey "Dude", what\'s up?'
