8df78654d87d9725954006eb8bc8f3b33f00b070
Bryan Lackey <bryan.lackey@gmail.com>
2015-01-20 19:03:10 -0500

    Added classes Person, User, Author, and GroceryList

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -1,58 +1,58 @@
 # User information
-def get_users_name
-	puts "What's your name?"
-	gets.chomp
-end
+# def get_users_name
+# 	puts "What's your name?"
+# 	gets.chomp
+# end
 
-def get_users_gender
-	puts "Gender [M or F]:"
-	gets.chomp
-end
+# def get_users_gender
+# 	puts "Gender [M or F]:"
+# 	gets.chomp
+# end
 
-def get_users_age
-	puts "How old are you?"
-	gets.chomp.to_i
-end
+# def get_users_age
+# 	puts "How old are you?"
+# 	gets.chomp.to_i
+# end
 
-def get_users_favorite_place
-	puts "Where's your favorite place to go?"
-	gets.chomp
-end
+# def get_users_favorite_place
+# 	puts "Where's your favorite place to go?"
+# 	gets.chomp
+# end
 
-def prints_user_message(user)		
-	puts "Hi #{user[:name]}, who is #{user[:age]} year(s) old!"
-	puts "Do you mind if I call you #{user[:name].chars.first}?"
-end
+# def prints_user_message(user)		
+# 	puts "Hi #{user[:name]}, who is #{user[:age]} year(s) old!"
+# 	puts "Do you mind if I call you #{user[:name].chars.first}?"
+# end
 
-def user_turns_75_message(user)
-	case user
-	when 0..74
-		puts "You will be 75 years old in #{75 - user} year(s)."
-	when 75
-		puts "You are #{user}!"
-	else
-		puts "You turned 75 years old #{user - 75} years ago."
-	end
-end
+# def user_turns_75_message(user)
+# 	case user
+# 	when 0..74
+# 		puts "You will be 75 years old in #{75 - user} year(s)."
+# 	when 75
+# 		puts "You are #{user}!"
+# 	else
+# 		puts "You turned 75 years old #{user - 75} years ago."
+# 	end
+# end
 
-def male?(user)
-	user == "M" || user == "Male"
-end
+# def male?(user)
+# 	user == "M" || user == "Male"
+# end
 
-def young_person_message(user)
-	young_person_greeting = male?(user) ? "boy" : "girl"
-	"young #{young_person_greeting}"
-end
+# def young_person_message(user)
+# 	young_person_greeting = male?(user) ? "boy" : "girl"
+# 	"young #{young_person_greeting}"
+# end
 
-def great_great_grandparent_message(user)
-	older_gender_greeting = male?(user) ? "pa" : "ma"
-	"great-great grand#{older_gender_greeting}"
-end
+# def great_great_grandparent_message(user)
+# 	older_gender_greeting = male?(user) ? "pa" : "ma"
+# 	"great-great grand#{older_gender_greeting}"
+# end
 
-def prints_user_age_message(user)
-	puts "You're a #{young_person_message(user[:gender])}." if user[:age] < 12
-	puts "You're probably someone's #{great_great_grandparent_message(user[:gender])}!!" if user[:age] >= 100
-end
+# def prints_user_age_message(user)
+# 	puts "You're a #{young_person_message(user[:gender])}." if user[:age] < 12
+# 	puts "You're probably someone's #{great_great_grandparent_message(user[:gender])}!!" if user[:age] >= 100
+# end
 
 def get_search_name
 	print "Search name: "
@@ -77,53 +77,179 @@ def pickup_item?(input)
 	input == "Y" || input == "yes"
 end
 
-def grocery_list_styling(list)
-	list.map.with_index { |item, index| "#{index + 1} -- #{item}" }
-end
-
 #User/Author information commands
-user = { name: get_users_name,     age: get_users_age, 
-	       gender: get_users_gender, fav_spot: get_users_favorite_place 
-}
+# user = { name: get_users_name,     age: get_users_age, 
+# 	       gender: get_users_gender, fav_spot: get_users_favorite_place 
+# }
 
-author  = { name: "Bryan", age: 25, gender: "M", fav_spot: "Yellowstone" }
-persons = [user, author]
+# author  = { name: "Bryan", age: 25, gender: "M", fav_spot: "Yellowstone" }
+# persons = [user, author]
 
-prints_user_message(user)
-user_turns_75_message(user[:age])
-prints_user_age_message(user)
+# prints_user_message(user)
+# user_turns_75_message(user[:age])
+# prints_user_age_message(user)
 
-search_name = get_search_name
-puts select_by_name(persons, search_name)
+# search_name = get_search_name
+# puts select_by_name(persons, search_name)
 
 #Grocery list commands
-grocery_list = ["potatoes", "bananas", "chicken", "eggs", "milk", "bacon", "flour"]
+# grocery_list = ["potatoes", "bananas", "chicken", "eggs", "milk", "bacon", "flour"]
 
-first_item_answer = gets_answer_about_groceries(grocery_list.first)
-grocery_list.shift if pickup_item?(first_item_answer)
+# first_item_answer = gets_answer_about_groceries(grocery_list.first)
+# grocery_list.shift if pickup_item?(first_item_answer)
 
-random_item = random_grocery_item(grocery_list)
-did_user_get_item = gets_answer_about_groceries(random_item) 
-grocery_list.delete(random_item) if pickup_item?(did_user_get_item)
+# random_item = random_grocery_item(grocery_list)
+# did_user_get_item = gets_answer_about_groceries(random_item) 
+# grocery_list.delete(random_item) if pickup_item?(did_user_get_item)
 
-puts "Oh yeah, don't forget the don't forget the bread!"
-grocery_list << "bread"
+# puts "Oh yeah, don't forget the don't forget the bread!"
+# grocery_list << "bread"
 
-styled_grocery_list = grocery_list_styling(grocery_list)
+# styled_grocery_list = grocery_list_styling(grocery_list)
 
-IO.write("grocery_list.txt", styled_grocery_list.join("\n"))
+# IO.write("grocery_list.txt", styled_grocery_list.join("\n"))
 
-new_grocery_list = IO.read("grocery_list.txt").split("\n")
+# new_grocery_list = IO.read("grocery_list.txt").split("\n")
 
-new_grocery_list.map! do |item|
-	item.scan(/[a-z]/).join("")
-end
+# new_grocery_list.map! do |item|
+# 	item.scan(/[a-z]/).join("")
+# end
 
-puts new_grocery_list
+# puts new_grocery_list
 
 # OOP Lab
 
 ## Add on to previous lab with:
+class Person
+	def initialize(args)
+		@name = args[:name] || "Anonymous"
+		@age = args[:age] || nil
+		@gender = args[:gender] || "unknown"
+	end
+end
+
+class User < Person
+	attr_reader :name, :gender
+	attr_accessor :age
+	def initialize(args)
+		super(args)
+		@fav_spot = args[:fav_spot] || "I don't have a favorite place."
+	end
+
+	def self.get_users_name
+		puts "What's your name?"
+		gets.chomp
+	end
+
+	def self.get_users_gender
+		puts "Gender [M or F]:"
+		gets.chomp
+	end
+
+	def self.get_users_age
+		puts "How old are you?"
+		gets.chomp.to_i
+	end
+
+	def self.get_users_favorite_place
+		puts "Where's your favorite place to go?"
+		gets.chomp
+	end
+
+	def prints_user_message
+		puts "Hi #{name}, who is #{age} year(s) old!"
+		puts "Do you mind if I call you #{name.chars.first}?"
+	end
+
+	def user_turns_75_message
+		case age
+		when 0..74
+			puts "You will be 75 years old in #{75 - age} year(s)."
+		when 75
+			puts "You are #{age}!"
+		else
+			puts "You turned 75 years old #{age - 75} years ago."
+		end
+	end
+
+	def male?
+		gender == "M" || gender == "Male"
+	end
+
+	def young_person_message
+		young_person_greeting = male? ? "boy" : "girl"
+		"young #{young_person_greeting}"
+	end
+
+	def great_great_grandparent_message
+		older_gender_greeting = male? ? "pa" : "ma"
+		"great-great grand#{older_gender_greeting}"
+	end
+
+	def prints_user_age_message
+		puts "You're a #{young_person_message}." if age < 12
+		puts "You're probably someone's #{great_great_grandparent_message}!!" if age >= 100
+	end
+end
+
+class Author < Person
+	def initialize(args)
+		super(args)
+		@fav_spot = args[:fav_spot]
+	end
+end
+
+class GroceryList
+	attr_accessor :grocery_list, :user
+	def initialize(grocery_list, user)
+		@grocery_list = IO.read(grocery_list).split("\n").map! { |item| item.scan(/[a-z]/).join("") }
+		@user = user 
+	end
+
+	def gets_answer_about_groceries
+		puts "Hey! did you get the #{grocery_list}? (Y/N)"
+		gets.chomp
+	end
+
+	def random_grocery_item
+		random_item = grocery_list.sample
+	end
+
+	def pickup_item?(input)
+		gets_answer_about_groceries == "Y" || gets_answer_about_groceries == "yes"
+	end
+
+	def grocery_list_styling
+		grocery_list.map.with_index { |item, index| "#{index + 1} -- #{item}" }
+	end
+
+	def owner
+		user
+	end
+
+	def to_s
+		"You need to pick up the following from the store: #{grocery_list}."
+	end
+end
+
+name = User.get_users_name
+age = User.get_users_age
+gender = User.get_users_gender
+fav_spot = User.get_users_favorite_place
+current_user = User.new(name: name, age: age, gender: gender, fav_spot: fav_spot)
+
+current_user.prints_user_message
+current_user.user_turns_75_message
+current_user.prints_user_age_message
+
+bryan_the_author = Author.new(name: "Bryan", age: 25, gender: gender, fav_spot: "Yellowstone")
+
+
+user = User.new ({ name: "Brittany", age: "23", gender: "F" })
+grocery_list = GroceryList.new("grocery_list.txt", user)
+puts grocery_list.owner.name
+puts grocery_list.to_s
+
 
 # Create User class
   # use class methods to gather user info
